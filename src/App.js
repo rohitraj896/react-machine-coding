ed337df9963216f1475ea58fcd817b73af772281
Rohit Kumar <rohitkr3390@gmail.com>
2024-05-05 12:58:44 +0530

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Pagination from "./components/Pagination/Pagination";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="flex justify-center gap-2 border border-black bg-green-300">
+        <a href="/">Home</a>
+        <a href="/pagination">Pagination</a>
+      </nav>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/pagination" element={<Pagination />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
