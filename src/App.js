ee41142b41c96a57e853733f9032915c13627644
Rohit Kumar <rohitkr3390@gmail.com>
2024-10-19 12:22:43 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Pagination from "./components/Pagination/Pagination";
 import Home from "./components/Home/Home";
 import ProgressBar from "./components/Progressbar/ProgressBar";
@@ -29,82 +29,74 @@ import CustomHook from "./components/CustomHook/CustomHook";
 function App() {
   return (
     <div>
-      <nav className="flex flex-wrap gap-2 border border-black bg-green-300">
-        <a href="/">Home</a>
-        <a href="/pagination">Pagination</a>
-        <a href="/progress-bar">Progress-bar</a>
-        <a href="/todo">Todo</a>
-        <a href="/type-checking">Type checking</a>
-        <a href="/star-rating">Star Rating</a>
-        <a href="/infinite-scroll">Infinite scroll</a>
-        <a href="/accordion">Accordion</a>
-        <a href="/modal">Modal</a>
-        <a href="/counter">Counter</a>
-        <a href="/carousel">Carousel</a>
-        <a href="/debounced-input">Debounced-input</a>
-        <a href="/debounced-api">Debounced-api</a>
-        <a href="/error-boundary">Error Boundary</a>
-        <a href="/flight-booking">Flight booking</a>
-        <a href="/stop-watch">Stop watch</a>
-        <a href="/traffic-signal">Traffic Signal</a>
-        <a href="/multi-lang">Multi Lang</a>
-        <a href="/session-timeout">Session Timeout</a>
-        <a href="/upload-file-progress">Upload file progress</a>
-        <a href="/dark-mode">Dark mode</a>
-        <a href="/dark-mode-capi">Dark mode CAPI</a>
-        <a href="/forward-ref">Forward Ref</a>
-        <a href="/hoc">HOC</a>
-        <a href="/custom-hook">Custom Hook</a>
-      </nav>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route
-              path="/error-boundary"
-              element={<ErrorBoundaryComponent />}
-            ></Route>
+      <BrowserRouter>
+        <nav className="flex flex-wrap gap-2 border border-black bg-green-300">
+          <Link href="/">Home</Link>
+          <Link href="/pagination">Pagination</Link>
+          <Link href="/progress-bar">Progress-bar</Link>
+          <Link href="/todo">Todo</Link>
+          <Link href="/type-checking">Type checking</Link>
+          <Link href="/star-rating">Star Rating</Link>
+          <Link href="/infinite-scroll">Infinite scroll</Link>
+          <Link href="/accordion">Accordion</Link>
+          <Link href="/modal">Modal</Link>
+          <Link href="/counter">Counter</Link>
+          <Link href="/carousel">Carousel</Link>
+          <Link href="/debounced-input">Debounced-input</Link>
+          <Link href="/debounced-api">Debounced-api</Link>
+          <Link href="/error-boundary">Error Boundary</Link>
+          <Link href="/flight-booking">Flight booking</Link>
+          <Link href="/stop-watch">Stop watch</Link>
+          <Link href="/traffic-signal">Traffic Signal</Link>
+          <Link href="/multi-lang">Multi Lang</Link>
+          <Link href="/session-timeout">Session Timeout</Link>
+          <Link href="/upload-file-progress">Upload file progress</Link>
+          <Link href="/dark-mode">Dark mode</Link>
+          <Link href="/dark-mode-capi">Dark mode CAPI</Link>
+          <Link href="/forward-ref">Forward Ref</Link>
+          <Link href="/hoc">HOC</Link>
+          <Link href="/custom-hook">Custom Hook</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/error-boundary"
+            element={<ErrorBoundaryComponent />}
+          ></Route>
 
-            <Route element={<ProtectedRoute />}>
-              <Route path="/pagination" element={<Pagination />}></Route>
-              <Route path="/progress-bar" element={<ProgressBar />}></Route>
-              <Route path="/todo" element={<Todo />}></Route>
-              <Route path="/type-checking" element={<TypeChecking />}></Route>
-              <Route path="/star-rating" element={<StarRating />}></Route>
-              <Route
-                path="/infinite-scroll"
-                element={<InfiniteScroll />}
-              ></Route>
-              <Route path="/accordion" element={<Accordion />}></Route>
-              <Route path="/modal" element={<ModalComponent />}></Route>
-              <Route path="/counter" element={<Counter />}></Route>
-              <Route path="/carousel" element={<Carousel />}></Route>
-              <Route path="/flight-booking" element={<FlightBooking />}></Route>
-              <Route
-                path="/debounced-input"
-                element={<InputSearchDebounce />}
-              ></Route>
-              <Route path="/debounced-api" element={<DebouncedApi />}></Route>
-              <Route path="/stop-watch" element={<StopWatch />}></Route>
-              <Route path="/traffic-signal" element={<TrafficSignal />}></Route>
-              <Route path="/multi-lang" element={<MultiLang />}></Route>
-              <Route
-                path="/session-timeout"
-                element={<SessionTimeout />}
-              ></Route>
-              <Route
-                path="/upload-file-progress"
-                element={<UploadFileProgress />}
-              ></Route>
-              <Route path="/dark-mode" element={<DarkMode />}></Route>
-              <Route path="/dark-mode-capi" element={<DarkModeCAPI />}></Route>
-              <Route path="/forward-ref" element={<ForwardRef />}></Route>
-              <Route path="/hoc" element={<HOC />}></Route>
-              <Route path="/custom-hook" element={<CustomHook />}></Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/pagination" element={<Pagination />}></Route>
+            <Route path="/progress-bar" element={<ProgressBar />}></Route>
+            <Route path="/todo" element={<Todo />}></Route>
+            <Route path="/type-checking" element={<TypeChecking />}></Route>
+            <Route path="/star-rating" element={<StarRating />}></Route>
+            <Route path="/infinite-scroll" element={<InfiniteScroll />}></Route>
+            <Route path="/accordion" element={<Accordion />}></Route>
+            <Route path="/modal" element={<ModalComponent />}></Route>
+            <Route path="/counter" element={<Counter />}></Route>
+            <Route path="/carousel" element={<Carousel />}></Route>
+            <Route path="/flight-booking" element={<FlightBooking />}></Route>
+            <Route
+              path="/debounced-input"
+              element={<InputSearchDebounce />}
+            ></Route>
+            <Route path="/debounced-api" element={<DebouncedApi />}></Route>
+            <Route path="/stop-watch" element={<StopWatch />}></Route>
+            <Route path="/traffic-signal" element={<TrafficSignal />}></Route>
+            <Route path="/multi-lang" element={<MultiLang />}></Route>
+            <Route path="/session-timeout" element={<SessionTimeout />}></Route>
+            <Route
+              path="/upload-file-progress"
+              element={<UploadFileProgress />}
+            ></Route>
+            <Route path="/dark-mode" element={<DarkMode />}></Route>
+            <Route path="/dark-mode-capi" element={<DarkModeCAPI />}></Route>
+            <Route path="/forward-ref" element={<ForwardRef />}></Route>
+            <Route path="/hoc" element={<HOC />}></Route>
+            <Route path="/custom-hook" element={<CustomHook />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
